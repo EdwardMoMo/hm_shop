af6015073afac73c8637f308cce2a0015cafb3e0
momanhong <momanhong@outlook.com>
2025-11-18 17:12:04 +0800

    完成首页基础布局

--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -1,4 +1,9 @@
 import "package:flutter/material.dart";
+import "package:hm_shop/components/Home/HmSlider.dart"; // Add the correct import path for HmSlider
+import "package:hm_shop/components/Home/HmCategory.dart"; // Add the correct import path for HmCategory
+import "package:hm_shop/components/Home/HmSuggestion.dart";
+import "package:hm_shop/components/Home/HmHot.dart";
+import "package:hm_shop/components/Home/HmMoreList.dart";
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -8,8 +13,35 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+
+  List<Widget> _getScrollChildren(){
+    return [
+      SliverToBoxAdapter(child: HmSlider()),
+      SliverToBoxAdapter(child: SizedBox(height: 10)),
+      SliverToBoxAdapter(child: HmCategory()),
+      SliverToBoxAdapter(child: SizedBox(height: 10)),
+      SliverToBoxAdapter(child: HmSuggestion()),
+      SliverToBoxAdapter(child: SizedBox(height: 10)),
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Flex(
+            direction: Axis.horizontal,
+            children: [
+             Expanded(child: HmHot()), 
+             SizedBox(width: 10),
+             Expanded(child: HmHot())],
+        ),
+        ),
+        ),
+        SliverToBoxAdapter(child: SizedBox(height: 10)),
+        HmMoreList(),
+
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("首页"));
+    return CustomScrollView(slivers: _getScrollChildren());
   }
 }
